b5c631409286f4f7d4bb9144280af1a7741a9a89
DESKTOP-TUH6J3I\kaushlender sharma <kaushalece81@gmail.com>
2022-07-09 21:15:02 +0530

    open api

---
+++ open-api-documentation-example/src/main/java/com/example/openapidocumentationexample/OpenApiDocumentationExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.openapidocumentationexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApiDocumentationExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenApiDocumentationExampleApplication.class, args);
+	}
+
+}
---
+++ open-api-documentation-example/src/test/java/com/example/openapidocumentationexample/OpenApiDocumentationExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openapidocumentationexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiDocumentationExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
