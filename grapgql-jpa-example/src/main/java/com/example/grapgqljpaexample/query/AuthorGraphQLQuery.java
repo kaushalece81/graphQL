c04edb735bb198b23ed6cb62ce8f779c20dbf80c
DESKTOP-TUH6J3I\kaushlender sharma <kaushalece81@gmail.com>
2022-07-10 19:03:16 +0530

    deleted exception handling

--- grapgql-jpa-example/src/main/java/com/example/grapgqljpaexample/query/AuthorGraphQLQuery.java
+++ grapgql-jpa-example/src/main/java/com/example/grapgqljpaexample/query/AuthorGraphQLQuery.java
@@ -5,8 +5,6 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import com.example.grapgqljpaexample.exception.ResourceNotFoundException;
 import com.example.grapgqljpaexample.response.AuthorResponse;
 import com.example.grapgqljpaexample.service.AuthorService;
 
@@ -17,12 +15,6 @@ public class AuthorGraphQLQuery implements GraphQLQueryResolver{
 	@Autowired
 	private AuthorService authorService;
 	public List<AuthorResponse> authors() {
-//		if(true) {
-//			throw new ResourceNotFoundException("Author Not found");
-//			//throw new GraphQLException("Client Not Available");
-//			//throw new RuntimeException("Internal server error Occured");
-//		}
-		
 		//return Collections.singletonList(AuthorResponse.builder().id(UUID.randomUUID()).name("kaushal").email("kaushal@gmail.com").build());
 		
 		// return from database
